919d57e38d23406b50b5f3f7a07fe108144e436d
Thiago Copetti <thiago.rc2009@hotmail.com>
2025-06-04 21:32:08 -0300

    Update inputdados.c

--- Códigos/inputdados.c
+++ Códigos/inputdados.c
@@ -4,6 +4,7 @@ int main()
 {
 	int idade;
 	double salario, altura;
+    char nome[50];
 	
 	
 	printf("Informe a idade :");
@@ -14,6 +15,9 @@ int main()
 	
 	printf("Informe a altura : ");
 	scanf("%lf", &altura);
+
+    printf("Informe o nome : ");
+    scanf("%s\n", nome); // somente para nome continuo, sem espaços. Não se usa o & antes da variável nome, pois ela já é um ponteiro para o primeiro elemento do array.
 	
 	printf("Idade : %d\n", idade);
     printf("Altura : %.2lf\n", altura);
